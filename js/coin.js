743fcd7de0af251fde111581e483fe444a38ff66
zrodriguez <zrodriguez@colgate.edu>
2018-04-09 18:19:46 -0400

    End testing.

--- js/coin.js
+++ js/coin.js
@@ -4,7 +4,7 @@ function createCoin() {
   var coinTexture = THREE.ImageUtils.loadTexture('images/GN.png');
   var materials = [];
 
-  materials.push(new THREE.MeshLambertMaterial({ color: 0xC19A6C }));
+  materials.push(new THREE.MeshLambertMaterial({ color: 0xC19A6B }));
   materials.push(new THREE.MeshLambertMaterial({ map: coinTexture, transparent: false, color: 0xC19A6B }));
   materials.push(new THREE.MeshLambertMaterial({ color: 0xC19A6B }));
   // materials.push(new THREE.MeshLambertMaterial({ color: 0xC19A6B }));
